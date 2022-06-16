f56ba73480679eab651ebfb0b5c803ae268f8e47
DeviReigns <89441895+DeviReigns@users.noreply.github.com>
2022-06-16 14:23:28 +0530

    Delete TOKENIZATION KERAS.py

--- TOKENIZATION KERAS.py
+++
@@ -1,9 +0,0 @@
-#pip install keras
-#pip install tensorflow
-import keras
-from keras.preprocessing.text import text_to_word_sequence
-# Create a string input
-str = "I love to study Natural Language Processing in Python"
-# tokenizing the text
-tokens = text_to_word_sequence(str)
-print(tokens)
