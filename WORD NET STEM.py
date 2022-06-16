33cd7de422eae6a3e624d2033fece140d4543778
DeviReigns <89441895+DeviReigns@users.noreply.github.com>
2022-06-16 14:24:13 +0530

    Delete WORD NET STEM.py

--- WORD NET STEM.py
+++
@@ -1,7 +0,0 @@
-from nltk.stem import WordNetLemmatizer
-lemmatizer = WordNetLemmatizer()
-print("word :\tlemma") 
-print("rocks :", lemmatizer.lemmatize("rocks"))
-print("corpora :", lemmatizer.lemmatize("corpora"))
-# a denotes adjective in "pos"
-print("better :", lemmatizer.lemmatize("better", pos ="a"))
