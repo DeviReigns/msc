a5445a78675cff0778fa71cbaf90d49f19904f89
DeviReigns <89441895+DeviReigns@users.noreply.github.com>
2022-06-16 14:23:39 +0530

    Delete TOKENIZATION NLTK.py

--- TOKENIZATION NLTK.py
+++
@@ -1,6 +0,0 @@
-import nltk
-from nltk.tokenize import word_tokenize
-# Create a string input
-str = "I love to study Natural Language Processing in Python"
-# Use tokenize method
-print(word_tokenize(str))
